794868e49faeb5a897ef2d4b3e6df7bde79972ed
Daniel Doña <daniel.dona@xnor.ga>
2019-12-18 10:38:57 +0100

    meh

---
+++ v0.1/id_generator.py
@@ -0,0 +1,190 @@
+from graphqlclient import GraphQLClient
+import time
+import json
+from random import randrange
+import argparse
+
+def generate_query_ids(querys, endpoints):
+	
+	decorated_querys = []
+
+	json_david = {}
+
+	for q in querys:
+
+		json_david[q["name"]] = {}
+
+		for e in endpoints:
+
+			ids = random_ids(q["ids_table"],20,e["url"])
+
+			json_david[q["name"]][e["name"]] = ids
+
+		q["values"] = json_david[q["name"]]
+
+		print(q["values"])
+
+		decorated_querys.append(q)
+
+	return json_david,decorated_querys
+
+def random_ids(table,n,endpoint):
+
+        client = GraphQLClient(endpoint)
+
+        get_max_id = '''query {
+          '''+str(table)+'''{ 
+                  nr
+          }
+        }'''
+
+        data = json.loads(client.execute(get_max_id))
+        max = int(data["data"][table][len(data["data"][table])-1]["nr"])
+        ids = []
+
+        for i in range(n):
+                ids.append(randrange(max))
+        return ids
+
+
+ep_1K = {"name": '1K', "url": 'http://localhost:4201/graphql'}
+ep_2K = {"name": '2K', "url": 'http://localhost:4202/graphql'}
+ep_4K = {"name": '4K', "url": 'http://localhost:4204/graphql'}
+ep_8K = {"name": '8K', "url": 'http://localhost:4208/graphql'}
+ep_16K = {"name": '16K', "url": 'http://localhost:4216/graphql'}
+
+
+
+
+q1 = {'name': 'q1', 'ids_table': 'listOffer', 'query': '''
+	query offer_product_review {
+	  listOffer(nr: "$$id$$") {
+		productWithReviews {
+		  reviews {
+			title
+			text
+			reviewDate
+			publishDate
+			rating1
+			rating2
+		  }
+		}
+	  }
+	}
+	'''}
+	
+q2 = {'name': 'q2', 'ids_table': 'listProducer', 'query': '''
+	query producer_product_review {
+		listProducerWithProduct(nr:"$$id$$") {
+    			productWithReviews {
+      				reviews { title }
+    			}
+  		}
+	}
+	'''}
+	
+q3 = {'name': 'q3', 'ids_table': 'listReview', 'query': '''
+query review_product_producttype_parenttype {
+  listReview(nr: "$$id$$") {
+    reviewFor {
+      producttype {
+        nr
+        label
+        comment
+        parent {
+          nr
+          label
+          comment
+        }
+      }
+    }
+  }
+}
+'''}
+
+q4 = {'name': 'q4', 'ids_table': 'listOffer', 'query': '''
+query offer_product_review_person_country {
+  listOffer(nr: "$$id$$") {
+    productWithReviews {
+      label
+      comment
+      reviews {
+        title
+        text
+        rating1
+        rating2
+        rating3
+        rating4
+        reviewer {
+          country {
+            code
+          }
+        }
+      }
+    }
+  }
+}
+'''}
+
+q5 = {'name': 'q5', 'ids_table': 'listProductWithReviews', 'query': '''
+query product_review_product {
+  listProductWithReviews(nr: "$$id$$") {
+    reviews {
+      title
+      reviewFor {
+        productWithReviews {
+          reviews {
+            title
+            reviewFor {
+              productWithReviews {
+                reviews {
+                  title
+                  reviewFor {
+                    product {
+                      label
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}	
+'''}
+
+q6 = {'name': 'q6', 'ids_table': 'listVendorWithOffers', 'query': '''
+query vendor_offer_product_producer_country {
+  listVendorWithOffers(nr: "$$id$$") {
+    offers {
+      product {
+        producer {
+          country {
+            code
+          }
+        }
+      }
+    }
+  }
+}	
+'''}
+
+querys = [q1,q2,q3,q4,q5,q6]
+
+#querys = [q6]
+
+endpoints = [ep_1K, ep_2K, ep_4K, ep_8K, ep_16K]
+
+#endpoints = [ep_1K]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--output_file", required=True, help="Output file with the random generated IDs for the LinGBM querys")
+args = parser.parse_args()
+    
+
+json_d,querys = generate_query_ids(querys, endpoints)
+
+with open(args.output_file, 'w') as fp:
+    json.dump(json_d, fp)
