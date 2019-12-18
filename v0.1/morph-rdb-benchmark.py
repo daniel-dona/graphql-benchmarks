2af8d57abd274b0910d2c6f421a487f651293ab5
Daniel Doña <daniel.dona@xnor.ga>
2019-12-18 12:07:58 +0100

    mié dic 18 12:07:58 CET 2019

--- v0.1/morph-rdb-benchmark.py
+++ v0.1/morph-rdb-benchmark.py
@@ -13,9 +13,9 @@ args = parser.parse_args()
 
 def gen_config(query,port):
 	
-	mapping = "/LinGBM.r2rml.ttl"
+	mapping = "LinGBM.r2rml.ttl"
 	
-	run_file = open("./tmp/morph.props", "w")
+	run_file = open("tmp/morph.props", "w")
 	
 	name = run_file.name
 	
