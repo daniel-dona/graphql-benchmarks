a4d3b6638680efc32a249b8f59940560f0a5e8b6
Daniel Doña <daniel.dona@xnor.ga>
2019-12-18 20:43:53 +0100

    Ready!

--- v0.1/csv_fixer.py
+++ v0.1/csv_fixer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-f = open("test.csv", "r")
+f = open("results/2/morph-rdb.csv", "r")
 for x in f:
 	v = x.replace("\n", "").split(",")
 	print(str(v[0])+","+str(v[1])+","+str(v[2])+","+str(int(v[3])/1000))
