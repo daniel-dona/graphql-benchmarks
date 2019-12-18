da4130ab66cd983416213fce2ef57e7aee9766ea
Daniel Doña <daniel.dona@xnor.ga>
2019-12-18 11:19:02 +0100

    mié dic 18 11:19:02 CET 2019

--- v0.1/morph-rdb-benchmark.py
+++ v0.1/morph-rdb-benchmark.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import json
+import tempfile
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--ids_file", required=True, help="Input file with JSON encoded IDs")
@@ -8,16 +9,45 @@ parser.add_argument("-o", "--output_timings", required=True, help="Output file w
 parser.add_argument("-d", "--debug_querys", required=False, help="Output file with debug data of the run querys")
 args = parser.parse_args()
 
+def gen_config(mapping,query,output,port):
+	
+	query_file = tempfile.NamedTemporaryFile()
+	
+	props = '''mappingdocument.file.path='''+mapping+'''example1-mapping-mysql.ttl
+	query.file.path='''+query+'''example1-query01.rq
+	output.file.path='''+output+'''example1-query01-result-mysql.xml
+
+	no_of_database=1
+	database.name[0]=benchmark
+	database.driver[0]=com.mysql.cj.jdbc.Driver
+	database.url[0]=jdbc:mysql://mysql-morph:'''+port+'''/morph_example?allowPublicKeyRetrieval=true&useSSL=false
+	database.user[0]=testing
+	database.pwd[0]=1234
+	database.type[0]=mysql'''
+	
+	query_file.write(props)
+
+def get_port(dataset):
+	
+	eps = {"1K": '3306', "2K": '3307', "4K": '3308', "8K": '3309', "16K": '3310', }
+
+	return eps[dataset]
+	
+
 with open(args.ids_file) as fp:
     ids = json.load(fp)
 
+base_path = "querys"
+
 for q in ids:
 	
-	for e in q:
+	for e in ids[q]:
 		
-		for v_id in e:
+		for v_id in ids[q][e]:
 			
-			print(q,e,v_id)
+			query_path = base_path+"/"+e+"/"+q+"/"+v_id+".rq"
+			
+			conf = gen_config(lingbm_mapping,query_path,output_result_path,get_port(e))
 		
 # Descargar directorios con consultas
 # Mapear datasets a puertos
@@ -26,14 +56,4 @@ for q in ids:
 # Sacar datos para tiempos	
 			
 
-props = '''mappingdocument.file.path='''+path_mapping+'''example1-mapping-mysql.ttl
-query.file.path='''+path_query+'''example1-query01.rq
-output.file.path='''+path_output+'''example1-query01-result-mysql.xml
 
-no_of_database=1
-database.name[0]=benchmark
-database.driver[0]=com.mysql.cj.jdbc.Driver
-database.url[0]=jdbc:mysql://mysql-morph:'''+port_server+'''/morph_example?allowPublicKeyRetrieval=true&useSSL=false
-database.user[0]=testing
-database.pwd[0]=1234
-database.type[0]=mysql'''
