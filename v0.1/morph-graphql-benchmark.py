3da5fbd9949e9d4bacd49faad1ee6e52a8c95928
Daniel Doña <daniel.dona@xnor.ga>
2019-12-18 11:02:55 +0100

    mié dic 18 11:02:55 CET 2019

--- v0.1/morph-graphql-benchmark.py
+++ v0.1/morph-graphql-benchmark.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from graphqlclient import GraphQLClient
 import time
 import json
--- v0.1/morph-rdb-benchmark.py
+++ v0.1/morph-rdb-benchmark.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--ids_file", required=True, help="Input file with JSON encoded IDs")
+parser.add_argument("-o", "--output_timings", required=True, help="Output file with CSV encoded benchmark results")
+parser.add_argument("-d", "--debug_querys", required=False, help="Output file with debug data of the run querys")
+args = parser.parse_args()
+
 with open('ids.json') as json_file:
     ids = json.load(json_file)
 
