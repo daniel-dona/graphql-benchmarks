326c64b6555653ff2293740656697ba4650b4a93
Daniel Doña <daniel.dona@xnor.ga>
2019-12-18 16:01:32 +0100

    sakjhgua

--- v0.1/csv_fixer.py
+++ v0.1/csv_fixer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-
-	
 f = open("test.csv", "r")
 for x in f:
-  print(x.split(",") 
+	v = x.replace("\n", "").split(",")
+	print(str(v[0])+","+str(v[1])+","+str(v[2])+","+str(int(v[3])/1000))
+
+
