bd5050a00f4b11afe26a0d80d463a511e2d4e071
Daniel Doña <daniel.dona@xnor.ga>
2019-12-18 12:04:07 +0100

    mié dic 18 12:04:07 CET 2019

--- v0.1/morph-rdb-benchmark.py
+++ v0.1/morph-rdb-benchmark.py
@@ -24,7 +24,7 @@ def gen_config(query,port):
 	output = output_result_path.name
 	
 	props = '''mappingdocument.file.path='''+mapping+'''
-	query.file.path=../'''+query+'''
+	query.file.path='''+query+'''
 	output.file.path='''+output+'''
 
 	no_of_database=1
